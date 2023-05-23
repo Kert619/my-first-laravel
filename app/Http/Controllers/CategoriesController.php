36bdf31263dd5cd7987a3e1153c986c13fcdfa38
Kert Tatoy <tatoy.kert.e@gmail.com>
2023-05-23 14:58:07 +0800

    Changes in products

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -9,40 +9,44 @@ use App\Models\CategoriesModel;
 class CategoriesController extends Controller
 {
     //
-    public function search(Request $request){
-       $search = $request->input('search');
-       $categories = CategoriesModel::where('category_name', 'like', '%' . $search . '%')->get();
+    public function search(Request $request)
+    {
+        $search = $request->input('search');
+        $categories = CategoriesModel::where('category_name', 'like', '%' . $search . '%')->get();
         return view('Categories/categories', ['categories' => $categories]);
     }
 
-    public function get($id){
-        $category= CategoriesModel::findorfail($id);
-        if(!$category){
+    public function get($id)
+    {
+        $category = CategoriesModel::findorfail($id);
+        if (!$category) {
             return response()->json(['message' => 'category not found'], 404);
         }
         return response()->json($category, 200);
     }
 
-    public function save(Request $request){
+    public function save(Request $request)
+    {
         try {
             $data = [
                 'category_name' => $request->input('category_name')
             ];
-            
+
             $category = CategoriesModel::create($data);
-    
+
             return redirect()->back()->with('success', 'Category added successfully');
         } catch (\Exception $e) {
             return redirect()->back()->with('error', 'Failed to add the category');
         }
     }
 
-    public function update(Request $request, $id){
+    public function update(Request $request, $id)
+    {
 
         try {
             $category = CategoriesModel::find($id);
             $data = [
-            'category_name' => $request->input('category_name')
+                'category_name' => $request->input('category_name')
             ];
 
             $category->update($data);
@@ -51,17 +55,17 @@ class CategoriesController extends Controller
         } catch (\Exception $e) {
             return redirect()->back()->with('error', 'Failed to update the category');
         }
-
     }
 
-    public function delete($id){
-       try {
+    public function delete($id)
+    {
+        try {
             $category = CategoriesModel::find($id);
             $category->delete();
 
             return redirect()->back()->with('success', 'Category deleted successfully');
-       } catch (\Exception $e) {
+        } catch (\Exception $e) {
             return redirect()->back()->with('error', 'Failed to delete the category');
-       }
+        }
     }
 }
--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -10,63 +10,81 @@ use App\Models\CategoriesModel;
 class ProductsController extends Controller
 {
     //
-    public function welcome(){
+    public function welcome()
+    {
         return view("welcome");
     }
 
-    public function search(Request $request){
+    public function search(Request $request)
+    {
         $search = $request->input('search');
         $products = ProductsModel::select('products.*', 'categories.category_name')
-                                 ->join('categories', 'products.category_id', '=', 'categories.id')
-                                 ->where('product_name', 'like', '%' . $search . '%')
-                                 ->get();
+            ->join('categories', 'products.category_id', '=', 'categories.id')
+            ->where('product_name', 'like', '%' . $search . '%')
+            ->get();
         $categories = CategoriesModel::select('id', 'category_name')->get();
         return view("Products/products", ['products' => $products, 'categories' => $categories]);
     }
 
-    public function get($id){
-        $product= ProductsModel::with('category')->findorfail($id);
-        if(!$product){
+    public function get($id)
+    {
+        $product = ProductsModel::with('category')->findorfail($id);
+        if (!$product) {
             return response()->json(['message' => 'product not found'], 404);
         }
 
         $product->category_name = $product->category->category_name;
         unset($product->category); // Remove the category object from the product
-        
-        
+
+
         return response()->json($product, 200);
     }
 
-    public function save(Request $request){
-        $data = [
-            'product_name' => $request->input('product_name'),
-            'category_id' => $request->input('category_id'),
-            'product_price' => $request->input('product_price'),
-            'product_stocks' => $request->input('product_stocks')
-        ];
+    public function save(Request $request)
+    {
+        try {
+            $data = [
+                'product_name' => $request->input('product_name'),
+                'category_id' => $request->input('category_id'),
+                'product_price' => $request->input('product_price'),
+                'product_stocks' => $request->input('product_stocks')
+            ];
 
-        $product = ProductsModel::create($data);
-        
-        return redirect()->back()->with('success', 'Product added successfully');
+            $product = ProductsModel::create($data);
+
+            return redirect()->back()->with('success', 'Product added successfully');
+        } catch (\Exception $e) {
+            return redirect()->back()->with('error', 'Failed to add the product');
+        }
     }
 
-    public function update(Request $request, $id){
-        $product = ProductsModel::find($id);
-        $data = [
-            'product_name' => $request->input('product_name'),
-            'category_id' => $request->input('category_id'),
-            'product_price' => $request->input('product_price'),
-            'product_stocks' => $request->input('product_stocks')
-        ];
+    public function update(Request $request, $id)
+    {
+        try {
+            $product = ProductsModel::find($id);
+            $data = [
+                'product_name' => $request->input('product_name'),
+                'category_id' => $request->input('category_id'),
+                'product_price' => $request->input('product_price'),
+                'product_stocks' => $request->input('product_stocks')
+            ];
 
-        $product->update($data);
-        return redirect()->back()->with('success', 'Product updated successfully');
+            $product->update($data);
+            return redirect()->back()->with('success', 'Product updated successfully');
+        } catch (\Exception $e) {
+            return redirect()->back()->with('error', 'Failed to update the product');
+        }
     }
 
-    public function delete($id){
-        $product = ProductsModel::find($id);
-        $product->delete();
-        
-        return redirect()->back()->with('success', 'Product deleted successfully');
+    public function delete($id)
+    {
+        try {
+            $product = ProductsModel::find($id);
+            $product->delete();
+
+            return redirect()->back()->with('success', 'Product deleted successfully');
+        } catch (\Exception $e) {
+            return redirect()->back()->with('error', 'Failed to delete the product');
+        }
     }
 }
