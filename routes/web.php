252c75ce81a0b5c5a1690cacf512fd5f036fdda1
Kert Tatoy <tatoy.kert.e@gmail.com>
2023-05-25 10:19:23 +0800

    updated modal

--- routes/web.php
+++ routes/web.php
@@ -26,8 +26,8 @@ Route::put('/products/{id}', "ProductsController@update")->name('products.update
 Route::delete('/products/{id}', "ProductsController@delete")->name('products.delete');
 
 //CATEGORIES
-Route::get('/categories',"CategoriesController@search")->name('categories.search');
+Route::get('/categories', "CategoriesController@search")->name('categories.search');
 Route::get('/categories/{id}', "CategoriesController@get")->name('categories.get');
-Route::post('/categories','CategoriesController@save')->name("categories.save");
-Route::put('/categories/{id}',"CategoriesController@update")->name('categories.update');
+Route::post('/categories', 'CategoriesController@save')->name("categories.save");
+Route::put('/categories/{id}', "CategoriesController@update")->name('categories.update');
 Route::delete('/categories/{id}', "CategoriesController@delete")->name('categories.delete');
